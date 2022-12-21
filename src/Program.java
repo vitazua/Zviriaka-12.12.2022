abbf6001f1bb85f524590b3ea19d28c1129382bc
Vitalii Zviriaka <vitazua@gmail.com>
2022-12-21 20:40:40 +0200

    Change Program

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,6 @@
 public class Program {
 
     public static void main(String[] args) {
-
-        int x=5;
-        int y=6;
-        System.out.println("x=" + x + "; y=" + y);
+        System.out.println("Hello world");
     }
 }
